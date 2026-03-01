5e805e6ec5bec26891c76d587a091d10a651032c
Fayakun <fayakunachmadisa2010@gmail.com>
2026-03-01 14:55:33 +0700

    change api to railway

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
-  }
-})
+      "/api": {
+        target: "https://duta-edukasi-production.up.railway.app/products",
+        changeOrigin: true,
+      },
+    },
+  },
+});
